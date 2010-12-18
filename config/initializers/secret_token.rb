259edc97aca4e03fb29f383f53e2e061478c01ca
Shaun Hare <shaunhare@sdh-imac.home>
2010-12-18 21:49:26 +0000

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookmarks::Application.config.secret_token = 'a44cbb6abae13047682bb9fdd78b362efdcf889b58c05e77fe3f17cabc70b087626252064d0ade8818d4fa80c36af57003cd8c610f23c07629597ac26ce4f647'
